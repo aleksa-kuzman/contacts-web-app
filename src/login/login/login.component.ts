7e768c703c0fde427ae3d5c7ad55fd53dbbfb82b
Aleksa Kuzman <kuzmana1@mskcc.org>
2024-09-30 23:00:44 +0200

    add login component

--- src/login/login/login.component.ts
+++ src/login/login/login.component.ts
@@ -1,14 +1,18 @@
 import { Component } from '@angular/core';
+import {MatInputModule} from '@angular/material/input'; 
+import {  MatCardModule } from '@angular/material/card';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [
+    MatCardModule,
+    MatInputModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
 
-  
+
 
 }
